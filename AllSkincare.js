f6913abf38b379d12016a0502d1e4ca441e1f977
aniketparmar29 <aniketparmar068@gmail.com>
2022-09-30 16:42:53 +0530

    justupdate

--- AllSkincare.js
+++ AllSkincare.js
@@ -1,6 +1,16 @@
 import navbar from "./componet/navbar.js";
 let nav = document.getElementById("navbar");
-nav.innerHTML = navbar();
+nav.innerHTML=navbar();
+import offers from "./componet/offer.js";
+let offer = document.getElementById("offer");
+offer.innerHTML=offers();
+import search from "./componet/search.js";
+let searchs = document.getElementById("search");
+searchs.innerHTML=search();
+import footer from "./componet/footer.js";
+let sfooter = document.getElementById("footer");
+sfooter.innerHTML=footer();
+
 
 const getData = async () => {
   try {
